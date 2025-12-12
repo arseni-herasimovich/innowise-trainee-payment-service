34e0a488298c0169747e5d8d179a477a90d053e8
arseni-herasimovich <arseni.herasimovich@innowise.com>
2025-12-12 15:33:07 +0300

    feat: dao layer

---
+++ src/main/java/com/innowise/paymentservice/repository/PaymentRepository.java
@@ -0,0 +1,21 @@
+package com.innowise.paymentservice.repository;
+
+import com.innowise.paymentservice.entity.Payment;
+import com.innowise.paymentservice.enums.PaymentStatus;
+import org.springframework.data.mongodb.repository.Aggregation;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+public interface PaymentRepository extends MongoRepository<Payment, UUID> {
+
+    @Aggregation(pipeline = {
+            "{ $match: { timestamp: { $gte: ?0, $lte: ?1 }, status: ?2 } }",
+            "{ $group: { _id: null, total: { $sum: '$paymentAmount' } } }"
+    })
+    Long sumOfPayments(Instant from, Instant to, PaymentStatus status);
+
+    List<Payment> findByOrderId(String orderId);
+}
